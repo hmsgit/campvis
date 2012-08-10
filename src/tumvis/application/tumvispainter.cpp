30279d83b864f433942740ea053e0ba76171f45d
schultezub <schultezub@bb408c1c-ae56-11e1-83d9-df6b3e0c105e>
2012-08-10 15:37:34 +0000

    * added update_legal_header.py script for updating the legal header in all code files of the project  * ran the file => all TUMVis source files now contain the legal header
    git-svn-id: https://camplinux.in.tum.de/svn/campvis/trunk@231 bb408c1c-ae56-11e1-83d9-df6b3e0c105e

--- src/tumvis/application/tumvispainter.cpp
+++ src/tumvis/application/tumvispainter.cpp
@@ -1,3 +1,31 @@
+// ================================================================================================
+// 
+// This file is part of the TUMVis Visualization Framework.
+// 
+// If not explicitly stated otherwise: Copyright (C) 2012, all rights reserved,
+//      Christian Schulte zu Berge (christian.szb@in.tum.de)
+//      Chair for Computer Aided Medical Procedures
+//      Technische Universität München
+//      Boltzmannstr. 3, 85748 Garching b. München, Germany
+// 
+// The licensing of this softare is not yet resolved. Until then, redistribution in source or
+// binary forms outside the CAMP chair is not permitted, unless explicitly stated in legal form.
+// However, the names of the original authors and the above copyright notice must retain in its
+// original state in any case.
+// 
+// Legal disclaimer provided by the BSD license:
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// 
+// ================================================================================================
+
 #include "tumvispainter.h"
 
 #include "tgt/assert.h"
---
+++ src/tumvis/update_legal_header.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import getopt
+import string
+
+dirsToIgnore = ['.svn', 'debug', 'release', 'Debug', 'Release', 'ext']
+
+legal_notice_begin = """// ================================================================================================
+// 
+// This file is part of the TUMVis Visualization Framework.
+"""
+legal_notice = """// 
+// If not explicitly stated otherwise: Copyright (C) 2012, all rights reserved,
+//      Christian Schulte zu Berge (christian.szb@in.tum.de)
+//      Chair for Computer Aided Medical Procedures
+//      Technische Universität München
+//      Boltzmannstr. 3, 85748 Garching b. München, Germany
+// 
+// The licensing of this softare is not yet resolved. Until then, redistribution in source or
+// binary forms outside the CAMP chair is not permitted, unless explicitly stated in legal form.
+// However, the names of the original authors and the above copyright notice must retain in its
+// original state in any case.
+// 
+// Legal disclaimer provided by the BSD license:
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+"""
+legal_notice_end = """// 
+// ================================================================================================"""
+
+def updateLegalHeaderInFile(file):
+    # read file
+    f = open(file)
+    data = f.read()
+    f.close()
+
+    # look for existing legal notice
+    start = data.find(legal_notice_begin)
+    end = data.find(legal_notice_end)
+    newdata = ''
+    
+    if (start == -1 or end == -1):
+        # no legal notice found, add one
+        newdata = legal_notice_begin + legal_notice + legal_notice_end + '\n\n' + data
+
+    else:
+        # legal notice found, check whether a update is necessary:
+        start2 = start + len(legal_notice_begin)
+        oldNotice = data[start2:end]
+        if (oldNotice != legal_notice):
+            # update necessary
+            newdata = data[:start2] + legal_notice + data[end:]
+        else:
+            # no update necessary
+            return
+
+    # save changes
+    f = open(file, "w")
+    f.write(newdata)
+    f.close()
+    print('Updated "' + file + '"')
+    
+def walkDirectory(directory):
+    for root, dirs, files in os.walk(directory):
+        for dir in dirsToIgnore:
+            try:
+                dirs.remove(dir)
+            except ValueError:
+                pass
+        for file in files:
+            if (file.endswith('.cpp') or file.endswith('.h') or file.endswith('.vert') or file.endswith('.frag')):
+                updateLegalHeaderInFile(root + '\\' + file)
+
+if (len(sys.argv) > 1):
+    for file in sys.argv[1:]:
+        if (os.path.exists(file)):
+            walkDirectory(file)
+else:
+    walkDirectory(os.getcwd())
