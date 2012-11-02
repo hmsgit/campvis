bd7d78b467d6fd0b267bc1413252b28f799ef7c1
schultezub <schultezub@bb408c1c-ae56-11e1-83d9-df6b3e0c105e>
2012-11-02 14:54:52 +0000

    added Vis module cmake file (forgotten in last commit)
    git-svn-id: https://camplinux.in.tum.de/svn/campvis/trunk@341 bb408c1c-ae56-11e1-83d9-df6b3e0c105e

---
+++ modules/vis/vis.cmake
@@ -0,0 +1,15 @@
+# CMake file for vis module
+
+# Source files:
+FILE(GLOB ThisModSources RELATIVE ${ModulesDir}
+	modules/vis/pipelines/*.cpp
+	modules/vis/processors/*.cpp
+)
+
+# Header files (including GLSL files so that they'll appear in VS projects)
+FILE(GLOB ThisModHeaders RELATIVE ${ModulesDir}
+    modules/vis/glsl/*.frag
+    modules/vis/glsl/*.vert
+	modules/vis/pipelines/*.h
+	modules/vis/processors/*.h
+)
