6e2073bc868fc89fd1c5ed4d636ff0d32be8aefd
Christian Schulte zu Berge <christian.szb@in.tum.de>
2014-05-05 14:08:49 +0200

    Updated legal headers, authors list and README.md

--- modules/preprocessing/pipelines/morphologyfilterdemo.cpp
+++ modules/preprocessing/pipelines/morphologyfilterdemo.cpp
@@ -2,11 +2,11 @@
 // 
 // This file is part of the CAMPVis Software Framework.
 // 
-// If not explicitly stated otherwise: Copyright (C) 2012-2013, all rights reserved,
+// If not explicitly stated otherwise: Copyright (C) 2012-2014, all rights reserved,
 //      Christian Schulte zu Berge <christian.szb@in.tum.de>
 //      Chair for Computer Aided Medical Procedures
-//      Technische Universität München
-//      Boltzmannstr. 3, 85748 Garching b. München, Germany
+//      Technische Universitaet Muenchen
+//      Boltzmannstr. 3, 85748 Garching b. Muenchen, Germany
 // 
 // For a full list of authors and contributors, please refer to the file "AUTHORS.txt".
 // 
