0674df598584bdf2dcf6373447535435e9115244
Declara Denis <denis.declara@in.tum.de>
2015-01-17 13:46:24 +0100

    Modified CMake file to support Visual Studio

--- modules/cudaconfidencemaps/cudaconfidencemaps.cmake
+++ modules/cudaconfidencemaps/cudaconfidencemaps.cmake
@@ -1,41 +1,54 @@
 # CMake file for vis module
 
 IF(${ModuleEnabled})
-	# Find CUDA
+    # Find CUDA
     FIND_PACKAGE(CUDA REQUIRED)
 
     if(CUDA_FOUND)
-		# Source files:
-		FILE(GLOB ThisModSources RELATIVE ${ModulesDir}
-			modules/cudaconfidencemaps/core/*.cpp
-			modules/cudaconfidencemaps/pipelines/*.cpp
-			modules/cudaconfidencemaps/processors/*.cpp
-		)
+        # Source files:
+        FILE(GLOB ThisModSources RELATIVE ${ModulesDir}
+            modules/cudaconfidencemaps/core/*.cpp
+            modules/cudaconfidencemaps/pipelines/*.cpp
+            modules/cudaconfidencemaps/processors/*.cpp
+        )
 
-		# Header files (including GLSL files so that they'll appear in VS projects)
-		FILE(GLOB ThisModHeaders RELATIVE ${ModulesDir}
-			modules/cudaconfidencemaps/glsl/*.frag
-			modules/cudaconfidencemaps/glsl/*.geom
-			modules/cudaconfidencemaps/glsl/*.vert
-			modules/cudaconfidencemaps/pipelines/*.h
-			modules/cudaconfidencemaps/processors/*.h
-		)
+        # Header files (including GLSL files so that they'll appear in VS projects)
+        FILE(GLOB ThisModHeaders RELATIVE ${ModulesDir}
+            modules/cudaconfidencemaps/glsl/*.frag
+            modules/cudaconfidencemaps/glsl/*.geom
+            modules/cudaconfidencemaps/glsl/*.vert
+            modules/cudaconfidencemaps/pipelines/*.h
+            modules/cudaconfidencemaps/processors/*.h
+        )
 
-		# Build CUDA sources
-		set(CUDA_PROPAGATE_HOST_FLAGS OFF) # Otherwise -std=c++11 is passed, which does not work with cuda compilers
-		set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS};-O3)
-		file(GLOB cuda_SOURCES modules/cudaconfidencemaps/core/*.cu)
-		cuda_add_library(
-			CudaConfidenceMaps_CUDA
-			${cuda_SOURCES}
-			)
-		LIST(APPEND ThisModExternalLibs CudaConfidenceMaps_CUDA)
+        IF(NOT WIN32)
+            # Otherwise -std=c++11 is passed to the compiler on linux. However official
+            # c++11 support for cuda is only available in CUDA 7
+            set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+        ENDIF()
 
-		SET(ThisModShaderDirectories "modules/cudaconfidencemaps/glsl")
-		SET(ThisModDependencies base io)
-	else()
-		MESSAGE(FATAL_ERROR "Could not find CUDA SDK.")
-	endif()
+        IF(CMAKE_BUILD_TYPE MATCHES RELEASE)
+            # Enable optimizations when building a release version
+            set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS};-O3)
+        ENDIF()
+
+        # Build CUDA sources
+        file(GLOB cuda_SOURCES modules/cudaconfidencemaps/core/*.cu)
+        CUDA_ADD_LIBRARY(CudaConfidenceMaps_CUDA STATIC
+            ${cuda_SOURCES}
+            )        
+
+        # Make sure code can find the CUSP include files included with this module
+        set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS};"-I ${ThisModDir}/ext/cusplibrary-0.4.0")
+
+        LIST(APPEND ThisModExternalLibs CudaConfidenceMaps_CUDA)
+
+        SET(ThisModShaderDirectories "modules/cudaconfidencemaps/glsl")
+        SET(ThisModDependencies base io)
+    else()
+        MESSAGE(FATAL_ERROR "Could not find CUDA SDK.")
+    endif()
+
 ENDIF(${ModuleEnabled})
 
 SET(ThisModStatus EXPERIMENTAL)
